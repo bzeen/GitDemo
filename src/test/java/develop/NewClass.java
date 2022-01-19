ba8c8123c4f0ec5ca0ce47e2e16b357f8fa64641
Bushra Naqvi <b_zehra88@hotmail.com>
2022-01-19 14:32:52 -0500

    final commit by user Y

--- src/test/java/develop/NewClass.java
+++ src/test/java/develop/NewClass.java
@@ -5,5 +5,9 @@ public class NewClass {
 	public void NewFunction1() {
 		System.out.println("New code by User X");
 	}
+	
+	public void NewFunction2() {
+		System.out.println("New code by User Y");
+	}
 
 }
